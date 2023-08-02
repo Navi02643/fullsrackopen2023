171458891cc4f40dead10d4689179b5873aa3324
Navi026 <karlos123jun@hotmail.com>
2023-08-01 21:43:11 -0600

    Ejercicio 1.6 terminado (correccion en las sumas de comentarios)

--- src/index.js
+++ src/index.js
@@ -1,61 +1,49 @@
 import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
 
-const Buttons = (props) => {
-  return <button>{props["label"]}</button>;
-};
-
-const FeedBack = (props) => {
-  const buttons = props["info"].map((info, index) => {
-    return <Buttons key={index} label={info["label"]} />;
-  });
-
-  return (
-    <div>
-      <h1>{props["feedbacktext"]}</h1>
-      {buttons}
-    </div>
-  );
-};
-
-const Info = (props) => {
-  return (
-    <p>
-      {props["label"]} {props["number"]}
-    </p>
-  );
-};
-
-const Statics = (props) => {
-  const info = props["info"].map((info, index) => {
-    return <Info key={index} label={info["label"]} number={info["number"]} />;
-  });
-  return (
-    <div>
-      <h1>{props["name"]}</h1>
-      {info}
-    </div>
-  );
-};
-
 const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-  const info = {
-    feed: "give feedback",
-    staticsName: "statics",
-    staticsInfo: [
-      { label: "good", number: good },
-      { label: "neutral", number: neutral },
-      { label: "bad", number: bad },
-    ],
+  const [clicks, setClicks] = useState({ good: 0, neutral: 0, bad: 0 });
+
+  const handleGoodClick = () => {
+    console.log(clicks);
+    const newClicks = {
+      good: clicks["good"] + 1,
+      neutral: clicks["neutral"],
+      bad: clicks["bad"],
+    };
+    setClicks(newClicks);
   };
+
+  const handleNeutralClick = () => {
+    const newClicks = {
+      good: clicks["good"],
+      neutral: clicks["neutral"] + 1,
+      bad: clicks["bad"],
+    };
+    setClicks(newClicks);
+  };
+
+  const handleBadClick = () => {
+    const newClicks = {
+      good: clicks["good"],
+      neutral: clicks["neutral"],
+      bad: clicks["bad"] + 1,
+    };
+    setClicks(newClicks);
+  };
+
   return (
-    <div>
-      <FeedBack feedbacktext={info["feed"]} info={info["staticsInfo"]} />
-      <Statics name={info["staticsName"]} info={info["staticsInfo"]} />
-    </div>
+    <>
+      <h1>Give feedBack</h1>
+      <button onClick={handleGoodClick}>Good</button>
+      <button onClick={handleNeutralClick}>Neutral</button>
+      <button onClick={handleBadClick}>Bad</button>
+
+      <h1>statics</h1>
+      <p>good: {clicks["good"]}</p>
+      <p>neutral: {clicks["neutral"]}</p>
+      <p>bad: {clicks["bad"]}</p>
+    </>
   );
 };
 
