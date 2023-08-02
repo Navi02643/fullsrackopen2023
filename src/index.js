8293630af031f6c6215670c1ed46ac0cf6367823
Navi026 <karlos123jun@hotmail.com>
2023-08-01 22:03:53 -0600

    Ejercicio 1.7 terminado

--- src/index.js
+++ src/index.js
@@ -4,8 +4,26 @@ import { createRoot } from "react-dom/client";
 const App = () => {
   const [clicks, setClicks] = useState({ good: 0, neutral: 0, bad: 0 });
 
+  const calculateAll = () => {
+    return clicks.good + clicks.neutral + clicks.bad;
+  };
+
+  const calculateAverage = () => {
+    const total = calculateAll();
+    const average = (clicks.good - clicks.bad) / total;
+    return average || 0;
+  };
+  const calculatePositive = () => {
+    const total = calculateAll();
+    if (total === 0) {
+      return 0;
+    }
+    const percentage = (clicks.good / total) * 100;
+    return percentage;
+  };
+
   const handleGoodClick = () => {
-    console.log(clicks);
+    calculateAverage();
     const newClicks = {
       good: clicks["good"] + 1,
       neutral: clicks["neutral"],
@@ -43,6 +61,9 @@ const App = () => {
       <p>good: {clicks["good"]}</p>
       <p>neutral: {clicks["neutral"]}</p>
       <p>bad: {clicks["bad"]}</p>
+      <p>All: {calculateAll()}</p>
+      <p>Average: {calculateAverage()}</p>
+      <p>Positive: {calculatePositive()}</p>
     </>
   );
 };
